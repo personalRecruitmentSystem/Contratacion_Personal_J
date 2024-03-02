7fab2bcd49e56f4b80f49f3c8386ebf214f78963
JasonS71 <JSantillan.ss77@gmail.com>
2024-03-01 20:47:02 -0400

    todo ordenado ahora si!

--- conexion.php
+++ conexion.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 $con =new mysqli("localhost", "root", "","bd_contratacion");
 if ($con->connect_error)
@@ -6,12 +5,3 @@ if ($con->connect_error)
 
 // echo "Funciona!... supongo<br>";
  ?>
-=======
-<?php 
-$con =new mysqli("localhost", "root", "","bd_contratacion");
-if ($con->connect_error)
- die ("conexion fallida".$con->connect_error);
-
-// echo "Funciona!... supongo<br>";
- ?>
->>>>>>> 9299c02bffef70c87f1e56ca731e7cdfd3de8219
